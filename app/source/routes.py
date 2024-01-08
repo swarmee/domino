d3d1af102818b59f9a5605c55e761f4b8be2c45c
swarmee <swarmee@users.noreply.github.com>
2024-01-08 09:52:48 +0000

    uat

--- app/source/routes.py
+++ app/source/routes.py
@@ -7,30 +7,41 @@ from business_logic import process_request
 import time
 import pandas as pd
 import numpy as np
-    
+import datetime
+from log_setup import logger
+import random
+from dateutil.relativedelta import relativedelta
+
 
 router = APIRouter()
 
+def create_time_series():
+    time_series = []
+    y_value = 1000000
+
+    for month in range(48):
+        date = (datetime.datetime(2019, 1, 1) + relativedelta(months=+month)).strftime('%Y-%m-%d')
+        jump = random.choice([random.uniform(1, 2), random.uniform(100, 999900)]) * random.choice([2, 1,1,55])
+        y_value += jump
+        time_series.append({"ds": date, "y": int(y_value)})
+
+    return time_series
+
 
 @router.get("/summary",
-             tags=["Create"])
-async def read_root(count: int = Query(None),
-                    correlation_id: Optional[str] = Header(None),
+            tags=["Create"])
+async def read_root(correlation_id: Optional[str] = Header(None),
                     accept: Optional[str] = Header("application/json",
                                                    description="Media type(s) that is/are acceptable for the response."),
                     user_agent: Optional[Union[UserAgentEnum, str]] = Header(UserAgentEnum.curl,
                                                                              description="Information about the user agent originating the request.")):
     start_time = time.time()
-    df1 = pd.DataFrame(np.random.rand(count, count))
-    df3 = df1.mul(df1)
+    response = create_time_series()
     end_time = time.time()
     processing_time = end_time - start_time
+    logger.info(f'Processing time for the API route: {processing_time} seconds')
 
-    headers = {
-        "Processing-Time": str(processing_time)
-    }
-
-    return headers
+    return response
 
 
 # Define the metrics endpoint so it appears in the swagger page
